0ff442c412afa1c7dbec77df0f67a003bb2dfc77
francisco7j.contacto@gmail.com <francisco7j.contacto@gmail.com>
2023-10-14 02:46:54 -0300

    primer commit

---
+++ src/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    
+    <div className="container my-5">
+      <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" alt="Logo" className="logo" />
+      <h1 className="text-start mt-4 mb-0">TheMovieDB Search</h1>
+      <p className="text-start">Explora y busca tu próximo película en estreno</p>
+    </div>
+    
+  );
+}
+
+export default LandingPage;
